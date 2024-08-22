36e9e604d0fe8fde7ed461fd5ac7b4b4633f2e65
Muhammad Popoola <popoolama@yahoo.com>
2024-08-22 10:02:10 +0100

    schedule tasks test

--- src/main/java/com/stanbic/redbox/debit/service/Application.java
+++ src/main/java/com/stanbic/redbox/debit/service/Application.java
@@ -2,9 +2,11 @@ package com.stanbic.redbox.debit.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 //	@SpringBootApplication
